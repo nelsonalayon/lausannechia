3f674031046bb94df6bed3134d528107cdfa9237
Nelson Alayon <nelson.alayon@gmail.com>
2021-04-25 13:51:59 -0500

    first commit, installed styled components

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,18 @@
+import { Title } from './styles.js'
+
+const Navbar = () => {
+
+    return (
+        <div>
+            <p>Llamanos o agenda una cita</p>
+            <figure>
+                <img src = "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80">
+                </img>
+            </figure>
+        </div>
+        
+    )
+
+}
+
+export default Navbar
